8570a42eba71264e134dc78df427c154cb10fa44
Nihal Rahman <nr2320@nyu.edu>
2023-05-10 16:40:31 -0400

    JAVA Pong Network and DB setup
    This commit a database connection set up and also has a basic connection going where once 2 people connect, the game notices

---
+++ JavaProjectClient/src/javaprojectclient/JavaProjectClient.java
@@ -0,0 +1,166 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaprojectclient;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.*;
+import javax.swing.*;
+import java.util.*;
+/**
+ *
+ * @author nihalrahman
+ */
+public class JavaProjectClient implements Runnable{
+
+    static Socket sock;
+    static Scanner sin;
+    static PrintWriter sout;
+    static Thread t = new Thread();
+    static WaitingRoom wr;
+    
+    
+    public static void main(String[] args) {
+        JavaProjectClient client = new JavaProjectClient();
+        client.run();
+    }
+    
+    
+    @Override
+    public void run(){ 
+        new LoginInterface();
+    }
+    
+}
+
+
+class checkReady implements Runnable{
+    @Override
+    public void run(){
+        String messages;
+        try{
+            while(JavaProjectClient.sin.hasNext()){
+                if(JavaProjectClient.sin.nextLine().equals("Ready")){
+                    JavaProjectClient.t.interrupt();
+                    JavaProjectClient.wr.jf.setVisible(false);
+                    new GameGUI();
+                }
+            }
+        } catch(Exception e){}
+    }
+}
+
+
+class LoginInterface{
+    static JFrame jf = new JFrame();
+    static TextField user = new TextField();
+    static TextField server = new TextField();
+    
+    LoginInterface(){
+        jf = new JFrame("Login Screen");
+        JPanel jp = new JPanel();
+        jp.setLayout(new GridLayout(4, 2, 4,4));
+        
+        JLabel userLabel = new JLabel("Username:");
+        JLabel serverLabel = new JLabel("Server:");
+        Button submit = new Button("Submit");
+        submit.addActionListener(new ButtonPressed());
+        
+        jp.add(userLabel);
+        jp.add(user);
+        jp.add(serverLabel);
+        jp.add(server);
+        
+        jp.add(submit);
+        
+        jf.add(jp);
+        jf.setSize(500,500);
+        jf.setVisible(true);
+        
+    }
+}
+
+
+class ButtonPressed implements ActionListener{
+    @Override
+    public void actionPerformed(ActionEvent e){  
+        String username = LoginInterface.user.getText();
+        String server = LoginInterface.server.getText();
+
+        if(server != null){
+            try{
+                JavaProjectClient.sock = new Socket(server,5190);
+                JavaProjectClient.sin = new Scanner(JavaProjectClient.sock.getInputStream());
+                JavaProjectClient.sout = new PrintWriter(JavaProjectClient.sock.getOutputStream(), true);
+            } catch (IOException ex){
+            System.out.println("Sorry, Connection failed.");
+            }
+        }
+
+        JavaProjectClient.sout.println(username);
+        LoginInterface.jf.setVisible(false);
+        String user = "";
+        String wins = "";
+        String loss = "";
+        int counter = 0;
+        try{
+            while(JavaProjectClient.sin.hasNext()){
+                if(counter == 0){
+                   user = JavaProjectClient.sin.nextLine();
+                }
+                if(counter == 1){
+                   wins = JavaProjectClient.sin.nextLine();
+                }
+                if(counter == 2){
+                   loss = JavaProjectClient.sin.nextLine();
+                   break;
+                }
+                counter += 1;
+            }
+        } catch(Exception e1){
+            System.out.print(e1);
+        }
+        
+        new WaitingRoom(user, wins, loss);
+
+        checkReady isReady = new checkReady();
+        JavaProjectClient.t = new Thread(isReady);
+        JavaProjectClient.t.start();
+
+    }
+}
+
+class WaitingRoom{
+    
+    static JFrame jf = new JFrame("Waiting Room");
+    
+    WaitingRoom(String username, String numWins, String numLoss){
+        JPanel jp = new JPanel();
+        jp.setLayout(new GridLayout(4, 1, 4,4));
+        JLabel message = new JLabel("Waiting for another user to connect...");
+        JLabel user = new JLabel("Username: " + username);
+        JLabel wins = new JLabel("Number of Wins: " + numWins);
+        JLabel loss = new JLabel("Number of Losses: " + numLoss);
+        
+        jp.add(message);
+        jp.add(user);
+        jp.add(wins);
+        jp.add(loss);
+        
+        jf.add(jp);
+        jf.setSize(500,500);
+        jf.setVisible(true);
+        
+    }
+        
+}
+
+class GameGUI{
+    GameGUI(){
+        JFrame jf = new JFrame("Game On!");
+        jf.setSize(500,500);
+        jf.setVisible(true);
+    }
+}
---
+++ JavaProjectServer/src/javaprojectserver/JavaProjectServer.java
@@ -0,0 +1,129 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaprojectserver;
+
+import java.io.*;
+import java.net.*;
+import java.sql.*;
+import java.util.*;
+import java.time.LocalDateTime;
+
+public class JavaProjectServer{
+    private static final String URL = "jdbc:mariadb://localhost:3306/JPong";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "nihal1234";
+    private static final int PORT = 5190;
+    static int numConnects = 0;
+    
+    static Map<String, Socket> clients = new HashMap<>();
+    
+    public static void main(String[] args) {
+
+        Connection conn = null;
+        try{
+            conn = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            System.out.println("Connected to the database");
+        } catch(SQLException e){
+            System.out.println("Caught exception: "+e.toString());
+        }
+        try {
+            ServerSocket server = new ServerSocket(PORT);
+            while (true){
+                Socket client = server.accept(); //Blocking system call
+                System.out.println("Got a connection from: "+client.getInetAddress());
+                new ProcessConnection(client,conn).start();
+            }
+                
+        } catch (IOException ex) {
+            System.out.println("Unable to bind to port "+PORT);
+        }
+    }
+}
+class ProcessConnection extends Thread{
+    Socket client;
+    Connection conn;
+    ProcessConnection(Socket newClient, Connection newConn){
+        client = newClient;
+        conn = newConn;
+    }
+    @Override
+    public void run(){
+        try{
+            Scanner sin = new Scanner(client.getInputStream());
+            PrintStream sout = new PrintStream(client.getOutputStream());
+            String username = sin.nextLine();
+            
+            String[] userData = obtainUser(conn, username);
+            
+            JavaProjectServer.numConnects += 1;
+            System.out.println(JavaProjectServer.numConnects);
+            
+            sout.println(userData[0]);
+            sout.println(userData[1]);
+            sout.println(userData[2]);
+            sout.println(JavaProjectServer.numConnects);
+            
+            if(JavaProjectServer.numConnects == 2){
+                broadcast("Ready");
+            }
+            /*
+            client.close();
+            sin.close();
+            sout.close();
+            */
+        }catch(Exception e){
+            System.out.println(client.getInetAddress()+" disconnected");
+        }
+    }
+    
+    private String[] obtainUser(Connection conn, String username) throws SQLException {
+        int win = 0;
+        int loss = 0;
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM Leaderboard WHERE username=?");
+        stmt.setString(1, username);
+        ResultSet rs = stmt.executeQuery();
+        boolean isThere = rs.next();
+        if(!isThere){
+           stmt = conn.prepareStatement("INSERT INTO Leaderboard (username) VALUES (?)");
+           stmt.setString(1, username);
+           rs = stmt.executeQuery();
+           isThere = rs.next();
+           win = rs.getInt("numWins");
+           loss = rs.getInt("numLoss");
+        }
+        else{
+            win = rs.getInt("numWins");
+            loss = rs.getInt("numLoss");
+        }
+        
+        stmt.close();
+        rs.close();
+        String[] data = {username, String.valueOf(win), String.valueOf(loss)};
+        
+        JavaProjectServer.clients.put(username, client);
+        return data;
+    }
+    
+    
+    private void logConnection(String username, String ipAddress, Connection conn) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO LOGINS (username, ip_address, time) VALUES (?, ?, ?)");
+        stmt.setString(1, username);
+        stmt.setString(2, ipAddress);
+        stmt.setString(3, LocalDateTime.now().toString());
+        stmt.executeUpdate();
+        stmt.close();
+    }
+    
+    private static void broadcast(String message) {
+        for (Socket client : JavaProjectServer.clients.values()) {
+            try {
+                PrintStream sout = new PrintStream(client.getOutputStream());
+                sout.println(message);
+            } catch (IOException e) {
+                 System.out.println("Unable to broadcast message");
+            }
+        }
+    }
+}
